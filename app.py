5330148a0940f94360102a1c5a53086592995adc
Tim Brasko <timothy-brasko@uiowa.edu>
2019-10-10 22:57:14 -0500

    simple calculator before changing it to loan calculator

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', display="", pageTitle='My Calculator')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method =='POST':
+        form = request.form
+        numberOne = int(form['numOne'])
+        numberTwo = int(form['numTwo'])
+        calc = numberOne + numberTwo
+        return render_template('index.html', display=calc, pageTitle='My Calculator')
+    
+    return redirect('/')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
